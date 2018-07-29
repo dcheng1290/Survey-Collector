3653f5fdba308cf33715e0007caf5e79fd4956dd
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-29 16:06:56 -0700

    added production keys for heroku

---
+++ config/prod.js
@@ -0,0 +1,15 @@
+// prod.js - production keys here!!
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoUri: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+// module.exports = {
+//   googleClientID: '387859108773-9q2doig21r96nrlmt2o51r2k0l9rmgd2.apps.googleusercontent.com',
+//   googleClientSecret: 'mIWUX5QY2tZErhetirfcXCM3',
+//   mongoUri: 'mongodb://davidjai123:chobo555@ds143900.mlab.com:43900/payment-prod',
+//   cookieKey: '12312321asfsdfdasfadsfdasfa',
+// };
