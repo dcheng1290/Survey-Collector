d3a5ee094fa19729848915484fbf538269d05d34
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-29 15:23:20 -0700

    Added passport initialize, serialize and deserialize functions

--- server/index.js
+++ server/index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('../routes/authRoutes.js');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('../config/keys');
+require('../database//models/User');
 require('../services/passport');
-require('../database/index');
+
+mongoose.connect(
+  keys.mongoUri,
+  { useNewUrlParser: true },
+  () => console.log('Db has connected!')
+);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.json());
 
