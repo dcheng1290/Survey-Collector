fbebd1c65f61cdfb3a31501777ee109433cda264
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-30 17:07:05 -0700

    Updated package.JSON

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ billRoutes(app);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // Like our main.js file, or main.css file!
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
   // express will serve up index.html if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
