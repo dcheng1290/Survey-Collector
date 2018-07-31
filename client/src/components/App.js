7711135c38c2423a938bdb4ec2738e17ffbb376e
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-30 17:32:35 -0700

    Fixed client sides exact path for surveys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
           <div>
             <Header />
             <Route exact path="/" component={Landing} />
-            <Route path="/surveys" component={Dashboard} />
+            <Route exact path="/surveys" component={Dashboard} />
             <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
