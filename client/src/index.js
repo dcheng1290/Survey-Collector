e3216d6ac4df73618e8b87d247b570fc95e0373a
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-31 17:03:23 -0700

    changed property name email into recipient to match schema

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -7,10 +7,6 @@ import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers/index';
 import App from './components/App';
-
-// to test
-import axios from 'axios';
-window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
